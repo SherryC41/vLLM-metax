c57ed3a0a0d48e533595748c75b00bd9b65fb47c
sherryC41 <sherry.c.c41@gmail.com>
2025-12-19 13:58:29 +0800

    fix: fix dp trap
    Signed-off-by: sherryC41 <sherry.c.c41@gmail.com>

--- vllm_metax/patch/__init__.py
+++ vllm_metax/patch/__init__.py
@@ -4,5 +4,6 @@ from . import maca_visible_device
 from . import distributed
 from . import device_allocator
 from . import model_executor
+from . import dp_fix
 from . import oot
 from . import sample
