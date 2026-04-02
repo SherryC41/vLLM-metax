7080545beac6a423d045ccc9b1e56f7e5a18af6e
Hank <hcc.mayday@gmail.com>
2026-04-02 14:13:21 +0800

    [Bugfix] correctly set sync_scheduling to false by default
    Signed-off-by: Hank <hcc.mayday@gmail.com>

--- vllm_metax/platform.py
+++ vllm_metax/platform.py
@@ -264,12 +264,6 @@ class MacaPlatformBase(Platform):
             )
 
         # -------------------------------------------------------
-        # Disable async_scheduling
-        scheduler_config = vllm_config.scheduler_config
-        if scheduler_config is not None:
-            scheduler_config.async_scheduling = False
-
-        # -------------------------------------------------------
         # Append sparse attention op for Maca platform
         if compilation_config is not None:
             compilation_config._attention_ops.append("vllm::mx_sparse_attn_indexer")
@@ -595,6 +589,8 @@ class MacaPlatformBase(Platform):
         cls, parser: FlexibleArgumentParser | None = None
     ) -> None:
         """Pre-register and update Maca platform."""
+        if parser is not None:
+            parser.set_defaults(async_scheduling=False)
         register_attention_backends()
         # TODO(m01016): update cudagraph max capture size here
 
