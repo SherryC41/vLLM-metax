2cc5efe5a15e40d86f6b2f64051fc1f49ea1b132
Hank <hcc.mayday@gmail.com>
2026-05-22 11:45:03 +0800

    [mla][prefill] add flash_attn mla_prefill backends
    Signed-off-by: Hank <hcc.mayday@gmail.com>

---
+++ vllm_metax/v1/attention/backends/mla/prefill/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0
+# 2026 - Modified by MetaX Integrated Circuits (Shanghai) Co., Ltd. All Rights Reserved.
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+from vllm.v1.attention.backends.mla.prefill.base import MLAPrefillBackend
+from vllm.v1.attention.backends.mla.prefill.registry import MLAPrefillBackendEnum
+from vllm.v1.attention.backends.mla.prefill.selector import get_mla_prefill_backend
+
+__all__ = [
+    "MLAPrefillBackend",
+    "MLAPrefillBackendEnum",
+    "get_mla_prefill_backend",
+]
