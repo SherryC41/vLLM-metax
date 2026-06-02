391a63ce6971baf9024390726e4e82e26d090e62
Hank <hcc.mayday@gmail.com>
2026-06-02 06:38:24 +0000

    [BugFix] import error
    Signed-off-by: Hank <hcc.mayday@gmail.com>

--- vllm_metax/quant_config/__init__.py
+++ vllm_metax/quant_config/__init__.py
@@ -6,8 +6,7 @@ from . import (
     awq,  # noqa: F401
     awq_marlin,  # noqa: F401
     compressed_tensors,  # noqa: F401
-    gptq,  # noqa: F401
-    gptq_marlin,  # noqa: F401
+    auto_gptq,  # noqa: F401
     moe_wna16,  # noqa: F401
     compressed_tensors_moe,  # noqa: F401
 )
